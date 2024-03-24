8c7e6f756e74fdfaa8d32ac67780688fa30ba45f
kana <chocolate.bar.favorite@gmail.com>
2024-03-24 19:41:19 +0900

    add new team mate

--- src/main/java/kana/project/Main.java
+++ src/main/java/kana/project/Main.java
@@ -10,10 +10,12 @@ public class Main {
         userMap.put("Miyata", "I'm from Tokyo.I'm 31years old.");
         userMap.put("Suzuki", "I'm from Saitama.I'm 32 years old.");
         userMap.put("Hiragisawa", "I'm from KanagawaI'm 29years old.");
+        userMap.put("Sato","I'm from Okinawa.I'm 25 years old.");
 
         System.out.println(userMap.get("Miyata"));
         System.out.println(userMap.get("Suzuki"));
         System.out.println(userMap.get("Hiragisawa"));
+        System.out.println(userMap.get("Sato"));
 
         try {
             if (userMap.containsKey("Miyata")) {
